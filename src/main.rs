8d1e2e16029bacd8ee5795e8626f756602a578dd
SergioAle210 <98841969+SergioAle210@users.noreply.github.com>
2024-07-17 11:31:01 -0600

    feat: Draw Poligon-4 with green fill and white border with a hole

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() {
     draw_polygon1(&mut fb);
     draw_polygon2(&mut fb);
     draw_polygon3(&mut fb);
+    draw_polygon4(&mut fb);
 
     fb.save_as_bmp("output.bmp").unwrap();
 }
@@ -73,6 +74,52 @@ fn draw_polygon3(fb: &mut Framebuffer) {
 
     // Dibujar contorno
     draw_polygon_border(fb, &vertices, border_color);
+}
+
+fn draw_polygon4(fb: &mut Framebuffer) {
+    let outer_vertices = vec![
+        Vec3::new(413.0, 177.0, 0.0),
+        Vec3::new(448.0, 159.0, 0.0),
+        Vec3::new(502.0, 88.0, 0.0),
+        Vec3::new(553.0, 53.0, 0.0),
+        Vec3::new(535.0, 36.0, 0.0),
+        Vec3::new(676.0, 37.0, 0.0),
+        Vec3::new(660.0, 52.0, 0.0),
+        Vec3::new(750.0, 145.0, 0.0),
+        Vec3::new(761.0, 179.0, 0.0),
+        Vec3::new(672.0, 192.0, 0.0),
+        Vec3::new(659.0, 214.0, 0.0),
+        Vec3::new(615.0, 214.0, 0.0),
+        Vec3::new(632.0, 230.0, 0.0),
+        Vec3::new(580.0, 230.0, 0.0),
+        Vec3::new(597.0, 215.0, 0.0),
+        Vec3::new(552.0, 214.0, 0.0),
+        Vec3::new(517.0, 144.0, 0.0),
+        Vec3::new(466.0, 180.0, 0.0),
+    ];
+
+    let hole_vertices = vec![
+        Vec3::new(682.0, 175.0, 0.0),
+        Vec3::new(708.0, 120.0, 0.0),
+        Vec3::new(735.0, 148.0, 0.0),
+        Vec3::new(739.0, 170.0, 0.0),
+    ];
+
+    let border_color = Color::new(255, 255, 255); // Blanco
+    let fill_color = Color::new(0, 255, 0); // Verde
+    let background_color = fb.background_color.clone(); // Color de fondo
+
+    fb.draw_polygon_with_hole(
+        &outer_vertices,
+        &hole_vertices,
+        border_color.clone(),
+        fill_color,
+        background_color,
+    );
+
+    // Dibujar contorno
+    draw_polygon_border(fb, &outer_vertices, border_color.clone());
+    draw_polygon_border(fb, &hole_vertices, border_color);
 }
 
 fn draw_polygon_border(fb: &mut Framebuffer, vertices: &[Vec3], border_color: Color) {
